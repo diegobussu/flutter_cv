7414e6d8540aeed3a01b4ea4b5011e306f7d23a7
DontitoSama <jokonito06@gmail.com>
2024-05-07 16:15:25 +0200

    :pencil: add vertical scroll

--- lib/screens/experiences.dart
+++ lib/screens/experiences.dart
@@ -9,37 +9,39 @@ class ExperiencesPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildExperienceItem(
-              'assets/experience1.png',
-              'Concepteur Développeur',
-              'CPAM du Val-de-Marne (94)',
-              'Réalisation de projets nationaux (CNAM) avec Symfony 5 (PHP 7.2) et PostgreSQL, impliquant l\'utilisation d\'API, la gestion de la sécurité (accès aux applications) et la mise en production.',
-            ),
-            SizedBox(height: 20),
-            _buildExperienceItem(
-              'assets/experience2.png',
-              'Technicien Informatique',
-              'Nice Informatique (06)',
-              'Une boutique spécialisée dans la réparation et vente d\'ordinateurs depuis 2010. Gestion du stock, récupération de données perdues, stockage data client, désinfection.',
-            ),
-            SizedBox(height: 20),
-            _buildExperienceItem(
-              'assets/experience3.png',
-              'Agent de tri et de distribution',
-              'La Poste (P.A.L, Nice)',
-              'Chargement, déchargement de camions et tri des colis. Préparation et distribution des colis, mise en point relais.',
-            ),
-            SizedBox(height: 20),
-            _buildExperienceItem(
-              'assets/experience4.png',
-              'Agent de piste',
-              'Avia Partner (Aéroport de Nice)',
-              'Conduite d\'engins de transport de bagages sur la piste, chargement des bagages en soute, aide au stationnement de l\'avion.',
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildExperienceItem(
+                'assets/experience1.png',
+                'Concepteur Développeur',
+                'CPAM du Val-de-Marne (94)',
+                'Réalisation de projets nationaux (CNAM) avec Symfony 5 (PHP 7.2) et PostgreSQL, impliquant l\'utilisation d\'API, la gestion de la sécurité (accès aux applications) et la mise en production.',
+              ),
+              SizedBox(height: 20),
+              _buildExperienceItem(
+                'assets/experience2.png',
+                'Technicien Informatique',
+                'Nice Informatique (06)',
+                'Une boutique spécialisée dans la réparation et vente d\'ordinateurs depuis 2010. Gestion du stock, récupération de données perdues, stockage data client, désinfection.',
+              ),
+              SizedBox(height: 20),
+              _buildExperienceItem(
+                'assets/experience3.png',
+                'Agent de tri et de distribution',
+                'La Poste (P.A.L, Nice)',
+                'Chargement, déchargement de camions et tri des colis. Préparation et distribution des colis, mise en point relais.',
+              ),
+              SizedBox(height: 20),
+              _buildExperienceItem(
+                'assets/experience4.png',
+                'Agent de piste',
+                'Avia Partner (Aéroport de Nice)',
+                'Conduite d\'engins de transport de bagages sur la piste, chargement des bagages en soute, aide au stationnement de l\'avion.',
+              ),
+            ],
+          ),
         ),
       ),
     );
